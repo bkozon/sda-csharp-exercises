b14c9acfc458dea4a6771e6493b6a1916cc92175
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 14:43:04 +0100

    :construction: Dokończyć zadanie E09.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            //Dla numeru PESEL podanego jako tekst, sprawdź, czy ma on poprawną cyfrę kontrolną zgodnie z
+            //algorytmem podanym na stronie
+            //https://pl.wikipedia.org/wiki/PESEL#Cyfra_kontrolna_i_sprawdzanie_poprawno%C5%9Bci_numeru
+            //Liczby, przez które mnożymy poszczególne cyfry numeru PESEL nazywają się wagami.
+            //Utwórz tablicę, w której zapiszesz te wagi i wykorzystaj tę tablicę do sprawdzenia poprawności
+            //cyfry kontrolnej.
+            //cyfra kontrolna -checksum 
+
+            string pesel1;
+            string pesel2;
+            string pesel3;
+            
+            int [] PESEL = { 5,5,0,3,0,1,0,1,1,9,3 };
+
+
+
+        }
+        public static void ValidatePESEL(int[] PESEL)
+        {
+            int[] numberWeight = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+            int sum = 0;
+            for (int i = 0; i < PESEL.Length; i++)
+            {
+                sum += PESEL[i] * numberWeight[i];
+            }
+            int modulo = sum % 10;
+
+            int checksum = 10 - modulo;
         }
     }
 }
