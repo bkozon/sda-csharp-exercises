aa5a0e65c4b987905e1f376723907017457125fb
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 10:40:07 +0100

    :sparkles: Rozwiązano zadanie E03

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            //Napisz funkcję tworzącą ciąg arytmetyczny o podanych: długości, pierwszym elemencie, różnicy ciągu.
+            Console.WriteLine("Długość ciągu arytmetycznego:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pierwszy element ciągu");
+            int firstElement = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Rożnica między elementami ciągu:");
+            int difference = Convert.ToInt32(Console.ReadLine());
+            ArithmeticSequence(length, firstElement, difference);
+           
+
+        }
+        public static void ArithmeticSequence(int length, int firstElement, int difference)
+        {
+            int sum = firstElement;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(sum); 
+                sum += difference;
+            }
+
         }
     }
 }
