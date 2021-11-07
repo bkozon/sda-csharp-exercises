57c1329b5cac3e977e4ace0fc9a4815ce1bdb112
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 12:44:43 +0100

    :sparkles: Zadanie E06.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace sda_csharp_exercises
 {
@@ -6,7 +7,50 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            //Dla podanego ciągu znaków oznaczającego hasło, sprawdź, czy spełnia on następujące warunki
+            //złożoności hasła:
+
+            //• długość co najmniej 6 znaków
+            //• występują co najmniej jedna duża i jedna mała litera
+            //• występuje co najmniej jedna cyfra
+            //• występuje co najmniej jeden znak specjalny spośród następujących: &*$#
+
+            Console.WriteLine("Wpisz hasło:");
+            string password = Console.ReadLine();
+            int passwordLength = password.Length;
+            PasswordComplexity(password);
+
+        }
+        public static void PasswordComplexity(string password)
+        {
+            int passwordLength = password.Length;
+            if (passwordLength < 6)
+            {
+                Console.WriteLine("Podane hasło jest za krótkie (min. 6 znaków).");
+            }
+            if (!(password.Any(char.IsUpper) && password.Any(char.IsLower)))
+            {
+                Console.WriteLine("Hasło musi zawierać conajmniej jedną wielką i jedną małą literę.");
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                Console.WriteLine("Hasło musi zawierać conajmniej jedną cyfrę.");
+            }
+            int sum = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == '&' || password[i] == '*' || password[i] == '$' || password[i] == '#')
+                {
+                    sum++;
+                }
+            }
+            if (sum == 0)
+            {
+                Console.WriteLine("Hasło musi zawierać conajmniej jeden znak specjalny (&*$#).");
+            }
+
+
+
         }
     }
 }
