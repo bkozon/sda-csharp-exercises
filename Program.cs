5f32fd13895379a2fb98dad7b38c9aefa8dea640
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 11:34:44 +0100

    :construction: Pierwsza część zadania E05.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            //Napisz funkcję, która będzie wyliczała wartość funkcji silnia dla argumentu podanego przez
+            //użytkownika.
+            Console.WriteLine("Podaj liczbę:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Podana liczba powinna być >0.");
+            }
+            else
+            {
+                Console.WriteLine($"{number}!={Strong(number)}");
+            }
+        }
+       public static int Strong (int number)
+        {
+            int sum = 1;
+            for (int i = 0; i < number; i++)
+            {
+                sum *= number-i;
+                
+            }
+            return sum;
         }
     }
 }
