23c1ce5d7fc1a8d81a109a12a8f8a9ba4014b33e
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 14:07:12 +0100

    :construction: Zadanie E08 do zrobienia.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            //Dla podanej tablicy liczb naturalnych wypisz tylko niepowtarzające się liczby.
+            //Przykład:
+            //int[] numbers = { 2, 5, 1, 2, 5 };
+            //Wynik:
+            //2 5 1
+            //Podpowiedź:
+            //zauważ, że w tablicy będą tylko liczby naturalne, czyli >= 0.Duplikat możesz więc nadpisać
+            //dowolną liczbą ujemną.
+
+            int[] numbers = { 2, 5, 1, 2, 5 };
         }
+
+        public static void PrintUniqueNumbers(int[] numbersArray)
+       
+
     }
 }
