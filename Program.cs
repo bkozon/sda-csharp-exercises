5af5a9beb0655f72d63576586c819a3ef1945138
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 09:47:53 +0100

    :sparkles: Rozwiązano zadanie E01

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                int totalPrice = i * 5;
+                Console.WriteLine($"Liczba sztuk: {i} łączna cena: {totalPrice}zł");
+
+            }
         }
+
     }
 }
