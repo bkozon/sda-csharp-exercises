de62e608aa47f7a89247f1a58d17638b9640f67b
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-07 15:32:40 +0100

    :construction: Zadanie E10 do zrobienia

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            int[] numbers = { 2, 4, 1, 8, 9, 5 };
+            BubbleSort(numbers);
+        }
+        public static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                int temp = 0;
+                int temp2 = 0;
+                if (numbers[i]>numbers[i+1])
+                {
+                    temp = numbers[i]; 
+                    temp2 = numbers[i + 1];
+                    numbers[i] = numbers[i + 1];                                              
+                    numbers[i + 1] = temp;
+                }
+                Console.WriteLine(numbers[i]);
+            }
+            
         }
     }
 }
