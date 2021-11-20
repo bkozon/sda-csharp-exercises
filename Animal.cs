4edcc2edff70e18cd7c65ec23fa9961fdb876752
Barbara Kozon <kozon.barbara@gmail.com>
2021-11-20 13:12:05 +0100

    :sparkles: Zad E15

---
+++ Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sda_csharp_exercises
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+
+        public Animal(string name, string sound)
+        {
+            Name = name;
+            Sound = sound;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} wydaje dźwięk {Sound}.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sda_csharp_exercises
 {
@@ -6,7 +7,31 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-        
+            Cat cat = new Cat("Mruczuś");
+            cat.MakeSound();
+            Dog dog = new Dog("Fafik");
+            dog.MakeSound();
+
+            List<Animal> animals = new List<Animal>();
+            animals.Add(cat);
+            animals.Add(dog);
+
+            foreach (Animal a in animals)
+            {
+                a.MakeSound();
+                if (a is Cat)
+                {
+                    Console.WriteLine("To jest kotek.");
+                }
+                else if (a is Dog)
+                {
+                    Console.WriteLine("To jest piesek.");
+                }
+                else
+                {
+                    Console.WriteLine("Jakiś dziwny zwierz.");
+                }
+            }
         }
     }
 }
